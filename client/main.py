f4acd97e5eab83f8b8117d78c2fd4466df8bc96f
JF Abdo <jfabdo@gmail.com>
2023-06-22 14:00:16 -0500

    added config to main

---
+++ client/main.py
@@ -0,0 +1,17 @@
+
+
+import socket
+import tqdm
+import os
+
+f = open('.client.config')
+
+if f == 0:
+    SEPARATOR = '<SEPARATOR>'
+    BUFFER_SIZE = 4096
+    host = '192.168.1.101'
+    port = 19999
+    filename = 'data.thing'
+else:
+    for 
+    if ":" in 
