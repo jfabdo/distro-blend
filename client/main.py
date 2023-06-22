a0820463d1ff2fd6f81203746403266a3e643bac
JF Abdo <jfabdo@gmail.com>
2023-06-22 14:31:00 -0500

    completed config option

--- client/main.py
+++ client/main.py
@@ -1,17 +1,21 @@
-
-
 import socket
 import tqdm
 import os
 
-f = open('.client.config')
+f = open('client/.client.config')
 
-if f == 0:
-    SEPARATOR = '<SEPARATOR>'
-    BUFFER_SIZE = 4096
-    host = '192.168.1.101'
-    port = 19999
-    filename = 'data.thing'
-else:
-    for 
-    if ":" in 
+variables = {}
+variables['SEPARATOR'] = '<SEPARATOR>'
+variables['BUFFER_SIZE'] = '4096'
+variables['host'] = '192.168.1.101'
+variables['port'] = '19999'
+variables['filename'] = 'data.thing'
+for line in f.readlines():
+    if ':' not in line:
+        print('Error:')
+        print(line)
+        continue
+    var, val = line.split(':')
+    variables[var] = val
+    print(var + " = " + val)
+
