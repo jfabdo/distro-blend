1cafcc06707ee1730945c4a0213d8b4463992b51
JF Abdo <jfabdo@gmail.com>
2023-06-23 17:07:57 -0500

    added send back

--- redo.py
+++ redo.py
@@ -1,9 +1,9 @@
 import argparse
 import socket
-import subprocess
+# import subprocess
 import os
-import threading
-import time
+# import threading
+# import time
 import tqdm
 
 parser = argparse.ArgumentParser(description="Use -c for client or -s for server")
@@ -15,51 +15,20 @@ SEPARATOR = "<SEPARATOR>"
 BUFFER_SIZE = 4096 # send 4096 bytes each time step
 SERVER_PORT = 19999
 
-class render(threading.Thread):
+# class render(threading.Thread):
 
-    def render(self, filename):
-        self.rendering = True
-        fileroot = "".join(filename.split('.')[:-1])
-        os.mkdir(fileroot)
-        subprocess.run([variables['blender_path']+'blender', '-b', filename, '-o','//./' + fileroot + "/",'-E','CYCLES','-F','PNG','-a','--','--cycles-device','CUDA'])#'-f','1'])
+#     def render(self, filename):
+#         self.rendering = True
+#         fileroot = "".join(filename.split('.')[:-1])
+#         os.mkdir(fileroot)
+#         subprocess.run([variables['blender_path']+'blender', '-b', filename, '-o','//./' + fileroot + "/",'-E','CYCLES','-F','PNG','-a','--','--cycles-device','CUDA'])#'-f','1'])
 
-    def run(self):
-        pass
+#     def run(self):
+#         pass
 
 class commonality:
-    def sendfile(self,s,filename):
-        # get the file size
-        filesize = os.path.getsize(filename)
-        # send the filename and filesize
-        s.send(f"{filename}{SEPARATOR}{filesize}".encode())
-        # start sending the file
-        progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-        with open(filename, "rb") as f:
-            while True:
-                # read the bytes from the file
-                bytes_read = f.read(BUFFER_SIZE)
-                if not bytes_read:
-                    # file transmitting is done
-                    break
-                # we use sendall to assure transimission in 
-                # busy networks
-                s.sendall(bytes_read)
-                # update the progress bar
-                progress.update(len(bytes_read))
-        # close the socket
-        s.close()
+    def receivefile(self,client_socket):
 
-    def receivefile(self,s):
-        
-        # enabling our server to accept connections
-        # 5 here is the number of unaccepted connections that
-        # the system will allow before refusing new connections
-        s.listen(5)
-        # print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
-        # accept connection if there is any
-        client_socket, address = s.accept() 
-        # if below code is executed, that means the sender is connected
-        print(f"[+] {address} is connected.")
         # receive the file infos
         # receive using client socket, not server socket
         received = client_socket.recv(BUFFER_SIZE).decode()
@@ -83,16 +52,33 @@ class commonality:
                 f.write(bytes_read)
                 # update the progress bar
                 progress.update(len(bytes_read))
-
-        # close the client socket
-        client_socket.close()
-        # close the server socket
-        s.close()
-
+        
+        return filename
+    
+    def sendfile(self,s,filename):
+        # get the file size
+        filesize = os.path.getsize(filename)
+        # send the filename and filesize
+        s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+        # start sending the file
+        progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+        with open(filename, "rb") as f:
+            while True:
+                # read the bytes from the file
+                bytes_read = f.read(BUFFER_SIZE)
+                if not bytes_read:
+                    # file transmitting is done
+                    break
+                # we use sendall to assure transimission in 
+                # busy networks
+                s.sendall(bytes_read)
+                # update the progress bar
+                progress.update(len(bytes_read))
+        
 class client(commonality):
     def clientmode(self):
         # the ip address or hostname of the server, the receiver
-        host = "127.0.0.1"
+        host = "192.168.1.130"
         
         # the name of file we want to send, make sure it exists
         filename = "data.csv"
@@ -103,6 +89,8 @@ class client(commonality):
         print("[+] Connected.") 
        
         self.sendfile(s,filename)
+        self.receivefile(s)
+        s.close()
 
 class server(commonality):
     def servermode(self):
@@ -112,7 +100,26 @@ class server(commonality):
         s = socket.socket()
         # bind the socket to our local address
         s.bind((SERVER_HOST, SERVER_PORT))
-        self.receivefile(s)
+        # enabling our server to accept connections
+        # 5 here is the number of unaccepted connections that
+        # the system will allow before refusing new connections
+        
+        
+        while True:
+            s.listen(5)
+            # print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
+            # accept connection if there is any
+            client_socket, address = s.accept() 
+            # if below code is executed, that means the sender is connected
+            print(f"[+] {address} is connected.")
+            filename = self.receivefile(client_socket)
+            #render and return file in thread
+            # close the client socket
+
+            self.sendfile(client_socket,filename)
+            client_socket.close()
+        # close the server socket
+        s.close()
         
 if __name__ == "__main__":
     if config['client']:
